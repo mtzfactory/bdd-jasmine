dfdaa10af27bf93fe7c386b43409695499931aec
mtzfactory <ricardo.martinez.monje@gmail.com>
2017-09-23 10:36:01 +0200

    add examples

---
+++ README.md
@@ -0,0 +1,14 @@
+[![Skylab](https://github.com/Iggy-Codes/logo-images/blob/master/logos/skylab-56.png)](http://www.skylabcoders.com/)
+
+## Full Stack Web Development Bootcamp @Otoño2017
+
+### BDD ~ Behavior Driven Development
+
+1.- Challenge 1: [[enlace]][challenge1]
+    solución: [enlace](bdd-drinkabout.html)
+
+2.- Challenge 2: [[enlace]][challenge2]
+    solución: [enlace](bdd-rockpaperscissors.html)
+
+[challenge1]: https://github.com/juanmaguitar/exercises-katas-js/blob/master/DrinkAbout/README.md
+[challenge2]: https://github.com/juanmaguitar/exercises-katas-js/blob/master/RockPaperScissors/README.md
---
+++ bdd-rockpaperscissors/RockPaperScissorsSpec.js
@@ -0,0 +1,46 @@
+describe('RockPaperScissors', function() {
+  var rockPaperScissors; // nombre de la instancia.
+  var hands = ["ROCK", "PAPER", "SCISSORS"];
+  var solutions = {
+    rock_paper: 'PAPER wins',
+    rock_rock: 'tie',
+    rock_scissors: 'ROCK wins',
+    paper_rock: 'PAPER wins',
+    paper_paper:  'tie',
+    paper_scissors: 'SCISSORS wins',
+    scissors_paper: 'SCISSORS wins',
+    scissors_scissors: 'tie',
+    scissors_rock: 'ROCK wins'
+  };
+
+  beforeEach(function() {
+    rockPaperScissors = new RockPaperScissors();
+  });
+
+  it('should win someone... playing TWO players.', function() {
+    var player1 = hands[getRandomInt(0, 2)];
+    var player2 = hands[getRandomInt(0, 2)];
+
+    var res = rockPaperScissors.play(player1, player2);
+
+    expect(res).toBe(player1 + ' vs ' + player2 + ' => ' + solutions[player1.toLowerCase() + '_' + player2.toLowerCase()] + '!');
+  });
+
+  it('should win someone... playing ONE player.', function() {
+    var player1 = hands[getRandomInt(0, 2)];
+
+    var res = rockPaperScissors.play(player1);
+    var player2 = res.substring(res.indexOf('vs ') + 3, res.indexOf(' =>'));
+    // TODO improve with regex.
+
+    expect(res).toBe(player1 + ' vs ' + player2 + ' => ' + solutions[player1.toLowerCase() + '_' + player2.toLowerCase()] + '!');
+  });
+
+  it('should thow Error because no player 1 defined.', function() {
+
+    var res = rockPaperScissors.play();
+
+    expect(res).toThrowError('No player 1 is defined!');
+  });
+
+});
